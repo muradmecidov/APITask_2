61e62e33cc8a411125373feae28ecfb5f5521b9d
muradmecidov <muradmecidov1499@gmail.com>
2023-06-06 12:04:59 +0400

    APITask2

---
+++ Controllers/BrandsController.cs
@@ -0,0 +1,86 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WEB_API.DAL;
+using WEB_API.Entities;
+using WEB_API.Entities.Dtos.Brand;
+using WEB_API.Entities.Dtos.Cars;
+
+namespace WEB_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+
+        public BrandsController(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<GetBrandDto>> GetById(int id)
+        {
+            var color = await _context.Brands.FindAsync(id);
+            if (color == null)
+            {
+                return NotFound();
+            }
+            GetBrandDto brandDto = _mapper.Map<GetBrandDto>(color);
+
+
+            return Ok(brandDto);
+        }
+        [HttpGet]
+
+        public async Task<ActionResult<GetBrandDto>> GetAll()
+        {
+            var result = await _context.Brands.ToListAsync();
+            if (result.Count == 0) { return NotFound(); }
+            List<GetBrandDto> brandDtos = _mapper.Map<List<GetBrandDto>>(result);
+
+            return Ok(brandDtos);
+        }
+        [HttpPost]
+        public async Task<ActionResult> AddCars([FromBody] CreateBrandDto create)
+        {
+            Brand brand = _mapper.Map<Brand>(create);
+          
+            await _context.Brands.AddAsync(brand);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+        [HttpPut]
+        public async Task<IActionResult> Update(int id ,UpdateBrandDto update)
+        {
+            if (!ProductExists(id))
+            {
+                return NotFound();
+            }
+            Brand brand = _mapper.Map<Brand>(update);
+            _context.Brands.Update(brand);
+            await _context.SaveChangesAsync();
+            return Ok(brand);
+
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _context.Brands.FindAsync(id);
+            if (result is null) { return NotFound(); }
+            _context.Brands.Remove(result);
+            await _context.SaveChangesAsync();
+            return Ok();
+
+        }
+        private bool ProductExists(int id)
+        {
+            return (_context.Cars?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+    }
+}
---
+++ Controllers/ColorsController.cs
@@ -0,0 +1,83 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WEB_API.DAL;
+using WEB_API.Entities;
+using WEB_API.Entities.Dtos.Cars;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace WEB_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ColorsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+
+        public ColorsController(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<GetColorDto>> GetById(int id)
+        {
+            var color = await _context.Colors.FindAsync(id);
+            if (color == null)
+            {
+                return NotFound();
+            }
+            GetColorDto colorDto = _mapper.Map<GetColorDto>(color);
+
+            return Ok(colorDto);
+        }
+        [HttpGet]
+
+        public async Task<ActionResult<IEnumerable<GetColorDto>>> GetAll()
+        {
+            var result = await _context.Colors.ToListAsync();
+            if (result.Count == 0) { return NotFound(); }
+            List<GetColorDto> colors = _mapper.Map<List<GetColorDto>>(result);
+            return Ok(colors);
+        }
+        [HttpPost]
+        public async Task<ActionResult> AddCars(CreateColorDto create)
+        {
+            Color color = _mapper.Map<Color>(create);
+            await _context.Colors.AddAsync(color);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+        [HttpPut]
+        public async Task<IActionResult> Update(int id, UpdateColorDto update)
+        {
+            if (!ProductExists(id))
+            {
+                return NotFound();
+            }
+            Color color = _mapper.Map<Color>(update);
+            _context.Colors.Update(color);
+            await _context.SaveChangesAsync();
+            return Ok(color);
+
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _context.Colors.FindAsync(id);
+            if (result is null) {  return NotFound(); }
+            _context.Colors.Remove(result);
+            await _context.SaveChangesAsync();
+            return Ok();
+
+        }
+        private bool ProductExists(int id)
+        {
+            return (_context.Cars?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+
+    }
+}
